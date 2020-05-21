af568cf6749bbed5c962dfc8f9db3922f4518a26
Jonathan George <jon@jonathangeorge.co.uk>
2020-05-22 00:02:46 +0100

    Added some basic specs for OpenApiDocumentProvider, as well as a failing test illustrating https://github.com/menes-dotnet/Menes/issues/39

---
+++ Solutions/Menes.Specs/Features/OpenApiDocumentProvider.feature
@@ -0,0 +1,39 @@
+﻿@perScenarioContainer
+Feature: OpenApiDocumentProvider
+	In order to route requests to OpenApi operations
+	As a developer
+	I want to load my OpenApi definition and use it to match requests to operations
+
+Scenario: Load an OpenApi document
+	Given I load an OpenApi document from the embedded resource 'Menes.Specs.Data.PetStore.yaml' and call it 'PetStore'
+	When I add the OpenApi document called 'PetStore' to the OpenApiDocumentProvider
+	Then the OpenApiDocumentProvider contains 1 document
+
+Scenario Outline: Match requests to operation path templates - success
+	Given I load an OpenApi document from the embedded resource 'Menes.Specs.Data.PetStore.yaml' and call it 'PetStore'
+	And I add the OpenApi document called 'PetStore' to the OpenApiDocumentProvider
+	When I get the operation path template for path '<Path>' and method '<Method>'
+	Then an operation template is returned
+	And the operation template has operation Id '<Expected Operation Id>' 
+
+	Examples:
+	| Description                 | Path              | Method | Expected Operation Id |
+	| GET with path parameter     | /pet/65           | GET    | getPetById            |
+	| POST without path parameter | /pet              | POST   | addPet                |
+	| PUT without path parameter  | /pet              | PUT    | updatePet             |
+	| POST with path parameter    | /pet/65           | POST   | updatePetWithForm     |
+	| DELETE with path parameter  | /pet/65           | DELETE | deletePet             |
+	| GET with query parameter    | /pet/findByStatus | GET    | findPetsByStatus      |
+
+Scenario Outline: Match requests to operation path templates - failure
+	Given I load an OpenApi document from the embedded resource 'Menes.Specs.Data.PetStore.yaml' and call it 'PetStore'
+	And I add the OpenApi document called 'PetStore' to the OpenApiDocumentProvider
+	When I get the operation path template for path '<Path>' and method '<Method>'
+	Then no operation template is returned
+
+	Examples:
+	| Description                                              | Path                                 | Method |
+	| No matching path                                         | /dogs                                | GET    |
+	| Invalid method for the specified path                    | /pet                                 | GET    |
+	| GET with path parameter value that does not match schema | /pet/fenton                          | GET    |
+	| End of request path matches a specified path             | /this/is/unexpected/pet/findByStatus | GET    |
---
+++ Solutions/Menes.Specs/Steps/OpenApiDocumentProviderSteps.cs
@@ -0,0 +1,90 @@
+﻿// <copyright file="OpenApiDocumentProviderSteps.cs" company="Endjin">
+// Copyright (c) Endjin. All rights reserved.
+// </copyright>
+
+namespace Menes.Specs.Steps
+{
+    using Corvus.SpecFlow.Extensions;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.OpenApi.Models;
+    using NUnit.Framework;
+    using NUnit.Framework.Internal;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class OpenApiDocumentProviderSteps
+    {
+        private readonly ScenarioContext scenarioContext;
+
+        public OpenApiDocumentProviderSteps(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
+
+        [Given("I load an OpenApi document from the embedded resource '(.*)' and call it '(.*)'")]
+        public void GivenILoadAnOpenApiDocumentFromTheEmbeddedResourceAndCallIt(
+            string embeddedResourceName,
+            string documentName)
+        {
+            OpenApiDocument openApiDocument = OpenApiServiceDefinitions.GetOpenApiServiceFromEmbeddedDefinition(
+                typeof(OpenApiDocumentProviderSteps).Assembly,
+                embeddedResourceName);
+
+            this.scenarioContext.Set(openApiDocument, documentName);
+        }
+
+        [Given("I add the OpenApi document called '(.*)' to the OpenApiDocumentProvider")]
+        [When("I add the OpenApi document called '(.*)' to the OpenApiDocumentProvider")]
+        public void WhenIAddTheOpenApiDocumentCalledToTheOpenApiDocumentProvider(string documentName)
+        {
+            OpenApiDocumentProvider documentProvider = this.GetOpenApiDocumentProvider();
+            OpenApiDocument document = this.scenarioContext.Get<OpenApiDocument>(documentName);
+
+            documentProvider.Add(document);
+        }
+
+        [When("I get the operation path template for path '(.*)' and method '(.*)'")]
+        public void WhenIGetTheOperationPathTemplateForPathAndMethod(string path, string method)
+        {
+            OpenApiDocumentProvider documentProvider = this.GetOpenApiDocumentProvider();
+            documentProvider.GetOperationPathTemplate(path, method, out OpenApiOperationPathTemplate? template);
+            this.scenarioContext.Set(template);
+        }
+
+        [Then("the OpenApiDocumentProvider contains (.*) document")]
+        public void ThenTheOpenApiDocumentProviderContainsDocument(int expectedDocumentCount)
+        {
+            OpenApiDocumentProvider documentProvider = this.GetOpenApiDocumentProvider();
+            Assert.AreEqual(expectedDocumentCount, documentProvider.AddedOpenApiDocuments.Count);
+        }
+
+        [Then("an operation template is returned")]
+        public void ThenAnOperationTemplateIsReturned()
+        {
+            OpenApiOperationPathTemplate? template = this.scenarioContext.Get<OpenApiOperationPathTemplate?>();
+            Assert.IsNotNull(template);
+        }
+
+        [Then("no operation template is returned")]
+        public void ThenNoOperationTemplateIsReturned()
+        {
+            OpenApiOperationPathTemplate? template = this.scenarioContext.Get<OpenApiOperationPathTemplate?>();
+            Assert.IsNull(template);
+        }
+
+        [Then("the operation template has operation Id '(.*)'")]
+        public void ThenTheOperationTemplateHasOperationId(string expectedOperationId)
+        {
+            OpenApiOperationPathTemplate? template = this.scenarioContext.Get<OpenApiOperationPathTemplate?>();
+            Assert.AreEqual(expectedOperationId, template?.Operation.OperationId);
+        }
+
+        private OpenApiDocumentProvider GetOpenApiDocumentProvider()
+        {
+            IOpenApiDocumentProvider documentProvider = ContainerBindings.GetServiceProvider(this.scenarioContext)
+                .GetRequiredService<IOpenApiDocumentProvider>();
+
+            return (OpenApiDocumentProvider)documentProvider;
+        }
+    }
+}
