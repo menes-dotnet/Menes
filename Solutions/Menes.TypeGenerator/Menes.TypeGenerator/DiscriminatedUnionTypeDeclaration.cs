f9a144e27fef87e1777c51cc93f07dfcb1bd001d
Matthew Adams <matthew.adams@endjin.com>
2020-10-14 15:04:21 +0100

    Aded an initial implementation of the DiscriminatedUnionTypeDeclaration.
    This is currently essentially the same as the UnionTypeDeclaration, but adapted to use the structure of the discriminated union.
    
    It now needs to be modified to use the discriminator.

--- Solutions/Menes.TypeGenerator/Menes.TypeGenerator/DiscriminatedUnionTypeDeclaration.cs
+++ Solutions/Menes.TypeGenerator/Menes.TypeGenerator/DiscriminatedUnionTypeDeclaration.cs
@@ -7,7 +7,10 @@ namespace Menes.TypeGenerator
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    using SF = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
     /// <summary>
     /// A discriminated union of other types.
@@ -62,7 +65,239 @@ namespace Menes.TypeGenerator
         /// <inheritdoc/>
         public override TypeDeclarationSyntax GenerateType()
         {
-            throw new NotImplementedException();
+            var builder = new StringBuilder();
+            builder.AppendLine($"public readonly struct {this.Name} : Menes.IJsonValue");
+            builder.AppendLine("{");
+            builder.AppendLine($"    public static readonly {this.Name} Null = new {this.Name}(default(System.Text.Json.JsonElement));");
+
+            int index = 0;
+            foreach ((ITypeDeclaration unionType, string discriminatorValue) in this.typesInUnion.Values)
+            {
+                if (unionType.IsCompoundType)
+                {
+                    builder.AppendLine($"    private readonly Menes.JsonReference? item{index + 1};");
+                }
+                else
+                {
+                    builder.AppendLine($"    private readonly {unionType.GetFullyQualifiedName()}? item{index + 1};");
+                }
+
+                index++;
+            }
+
+            index = 0;
+
+            foreach ((ITypeDeclaration unionType, string discriminatorValue) in this.typesInUnion.Values)
+            {
+                builder.AppendLine($"    public {this.Name}({unionType.GetFullyQualifiedName()} clrInstance)");
+                builder.AppendLine("    {");
+                builder.AppendLine("        if (clrInstance.HasJsonElement)");
+                builder.AppendLine("        {");
+                builder.AppendLine("            this.JsonElement = clrInstance.JsonElement;");
+                builder.AppendLine($"            this.item{index + 1} = null;");
+                builder.AppendLine("        }");
+                builder.AppendLine("        else");
+                builder.AppendLine("        {");
+
+                if (unionType.IsCompoundType)
+                {
+                    builder.AppendLine($"            this.item{index + 1} = Menes.JsonReference.FromValue(clrInstance);");
+                }
+                else
+                {
+                    builder.AppendLine($"            this.item{index + 1} = clrInstance;");
+                }
+
+                builder.AppendLine("            this.JsonElement = default;");
+                builder.AppendLine("        }");
+
+                for (int i = 0; i < this.typesInUnion.Count; ++i)
+                {
+                    if (i == index)
+                    {
+                        continue;
+                    }
+
+                    builder.AppendLine($"        this.item{i + 1} = null;");
+                }
+
+                builder.AppendLine("    }");
+                index++;
+            }
+
+            builder.AppendLine("    public JsonUnionExample(System.Text.Json.JsonElement jsonElement)");
+            builder.AppendLine("    {");
+
+            for (int i = 0; i < this.typesInUnion.Count; ++i)
+            {
+                builder.AppendLine($"        this.item{i + 1} = null;");
+            }
+
+            builder.AppendLine("        this.JsonElement = jsonElement;");
+            builder.AppendLine("    }");
+
+            builder.Append("    public bool IsNull =>");
+
+            for (int i = 0; i < this.typesInUnion.Count; ++i)
+            {
+                if (i > 0)
+                {
+                    builder.Append(" &&");
+                }
+
+                builder.Append($" this.item{i + 1} is null");
+            }
+
+            builder.AppendLine(" && (this.JsonElement.ValueKind == System.Text.Json.JsonValueKind.Undefined || this.JsonElement.ValueKind == System.Text.Json.JsonValueKind.Null);");
+
+            builder.AppendLine($"    public {this.Name}? AsOptional => this.IsNull ? default({this.Name}?) : this;");
+
+            index = 0;
+
+            foreach ((ITypeDeclaration unionType, string discriminatorValue) in this.typesInUnion.Values)
+            {
+                string fullyQualifiedTypeName = unionType.GetFullyQualifiedName();
+                string fullyQualifiedTypeNameOrReference = unionType.IsCompoundType ? "Menes.JsonReference" : fullyQualifiedTypeName;
+                builder.AppendLine($"    public bool Is{unionType.Name} => this.item{index + 1} is {fullyQualifiedTypeNameOrReference} || {fullyQualifiedTypeName}.IsConvertibleFrom(this.JsonElement);");
+                index++;
+            }
+
+            builder.AppendLine("    public bool HasJsonElement => this.JsonElement.ValueKind != System.Text.Json.JsonValueKind.Undefined;");
+
+            builder.AppendLine("    public System.Text.Json.JsonElement JsonElement { get; }");
+
+            foreach ((ITypeDeclaration unionType, string discriminatorValue) in this.typesInUnion.Values)
+            {
+                string fullyQualifiedTypeName = unionType.GetFullyQualifiedName();
+                builder.AppendLine($"    public static explicit operator {fullyQualifiedTypeName}({this.Name} value) => value.As{unionType.Name}();");
+                builder.AppendLine($"    public static implicit operator {this.Name}({fullyQualifiedTypeName} value) => new {this.Name}(value);");
+            }
+
+            builder.AppendLine($"    public static {this.Name} FromOptionalProperty(in System.Text.Json.JsonElement parentDocument, System.ReadOnlySpan<char> propertyName) =>");
+            builder.AppendLine("        parentDocument.TryGetProperty(propertyName, out System.Text.Json.JsonElement property)");
+            builder.AppendLine($"            ? new {this.Name}(property)");
+            builder.AppendLine("            : Null;");
+
+            builder.AppendLine($"    public static {this.Name} FromOptionalProperty(in System.Text.Json.JsonElement parentDocument, string propertyName) =>");
+            builder.AppendLine("        parentDocument.TryGetProperty(propertyName, out System.Text.Json.JsonElement property)");
+            builder.AppendLine($"            ? new {this.Name}(property)");
+            builder.AppendLine("            : Null;");
+
+            builder.AppendLine($"    public static {this.Name} FromOptionalProperty(in System.Text.Json.JsonElement parentDocument, System.ReadOnlySpan<byte> utf8PropertyName) =>");
+            builder.AppendLine("        parentDocument.TryGetProperty(utf8PropertyName, out System.Text.Json.JsonElement property)");
+            builder.AppendLine($"            ? new {this.Name}(property)");
+            builder.AppendLine("            : Null;");
+
+            index = 0;
+            foreach ((ITypeDeclaration unionType, string discriminatorValue) in this.typesInUnion.Values)
+            {
+                string fullyQualifiedTypeName = unionType.GetFullyQualifiedName();
+
+                if (unionType.IsCompoundType)
+                {
+                    builder.AppendLine($"    public {fullyQualifiedTypeName} As{unionType.Name}() => this.item{index + 1}?.AsValue<{fullyQualifiedTypeName}>() ?? new {fullyQualifiedTypeName}(this.JsonElement);");
+                }
+                else
+                {
+                    builder.AppendLine($"    public {fullyQualifiedTypeName} As{unionType.Name}() => this.item{index + 1} ?? new {fullyQualifiedTypeName}(this.JsonElement);");
+                }
+
+                ++index;
+            }
+
+            builder.AppendLine("    public void WriteTo(System.Text.Json.Utf8JsonWriter writer)");
+            builder.AppendLine("    {");
+
+            index = 0;
+
+            foreach ((ITypeDeclaration unionType, string discriminatorValue) in this.typesInUnion.Values)
+            {
+                if (index > 0)
+                {
+                    builder.Append("else ");
+                }
+
+                string typeName = unionType.IsCompoundType ? "Menes.JsonReference" : unionType.GetFullyQualifiedName();
+
+                builder.AppendLine($"        if (this.item{index + 1} is {typeName} item{index + 1})");
+                builder.AppendLine("        {");
+                builder.AppendLine($"            item{index + 1}.WriteTo(writer);");
+                builder.AppendLine("        }");
+                index++;
+            }
+
+            builder.AppendLine("        else");
+            builder.AppendLine("        {");
+            builder.AppendLine("            this.JsonElement.WriteTo(writer);");
+            builder.AppendLine("        }");
+            builder.AppendLine("    }");
+
+            builder.AppendLine("    public override string? ToString()");
+            builder.AppendLine("    {");
+            builder.AppendLine("        var builder = new System.Text.StringBuilder();");
+            index = 0;
+
+            foreach ((ITypeDeclaration unionType, string discriminatorValue) in this.typesInUnion.Values)
+            {
+                string typeName = unionType.IsCompoundType ? "Menes.JsonReference" : unionType.GetFullyQualifiedName();
+
+                builder.AppendLine($"        if (this.Is{unionType.Name})");
+                builder.AppendLine("        {");
+                builder.AppendLine("            builder.Append(\"{\");");
+                builder.AppendLine($"            builder.Append(\"{unionType.Name}\");");
+                builder.AppendLine("            builder.Append(\", \");");
+                builder.AppendLine($"            builder.Append(this.As{unionType.Name}().ToString());");
+                builder.AppendLine("            builder.AppendLine(\"}\");");
+                builder.AppendLine("        }");
+                index++;
+            }
+
+            builder.AppendLine("        return builder.Length > 0 ? builder.ToString() : this.JsonElement.ToString();");
+
+            builder.AppendLine("    }");
+
+            builder.AppendLine("    public Menes.ValidationContext Validate(in Menes.ValidationContext validationContext)");
+            builder.AppendLine("    {");
+            builder.AppendLine("        if (this.IsNull)");
+            builder.AppendLine("        {");
+            builder.AppendLine("            return validationContext;");
+            builder.AppendLine("        }");
+
+            for (int i = 0; i < this.typesInUnion.Count; ++i)
+            {
+                builder.AppendLine($"        Menes.ValidationContext validationContext{i + 1} = Menes.ValidationContext.Root.WithPath(validationContext.Path);");
+            }
+
+            index = 0;
+            foreach ((ITypeDeclaration unionType, string discriminatorValue) in this.typesInUnion.Values)
+            {
+                builder.AppendLine($"        if (this.Is{unionType.Name})");
+                builder.AppendLine("        {");
+                builder.AppendLine($"            validationContext{index + 1} = this.As{unionType.Name}().Validate(validationContext{index + 1});");
+                builder.AppendLine("        }");
+                builder.AppendLine("        else");
+                builder.AppendLine("        {");
+                builder.AppendLine($"            validationContext{index + 1} = validationContext{index + 1}.WithError(\"The value is not convertible to a {unionType.GetFullyQualifiedName()}.\");");
+                builder.AppendLine("        }");
+                index++;
+            }
+
+            builder.Append("        return Menes.Validation.ValidateOneOf(validationContext");
+
+            index = 0;
+            foreach ((ITypeDeclaration unionType, string discriminatorValue) in this.typesInUnion.Values)
+            {
+                builder.Append(", ");
+                builder.Append($"(\"{unionType.GetFullyQualifiedName()}\", validationContext{index + 1})");
+                index++;
+            }
+
+            builder.AppendLine(");");
+
+            builder.AppendLine("    }");
+            builder.AppendLine("}");
+
+            return (TypeDeclarationSyntax)SF.ParseMemberDeclaration(builder.ToString());
         }
 
         /// <inheritdoc/>
