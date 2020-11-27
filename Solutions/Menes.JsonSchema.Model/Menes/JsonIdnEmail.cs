935bb307ae00ec93fc9a421b18435a3358df0c17
Matthew Adams <matthew.adams@endjin.com>
2020-11-27 00:35:06 +0000

    Added IDN email validation.

--- Solutions/Menes.JsonSchema.Model/Menes/JsonIdnEmail.cs
+++ Solutions/Menes.JsonSchema.Model/Menes/JsonIdnEmail.cs
@@ -6,6 +6,7 @@ namespace Menes
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Text.Json;
     using System.Text.RegularExpressions;
 
@@ -18,7 +19,6 @@ namespace Menes
         /// The null value.
         /// </summary>
         public static readonly JsonIdnEmail Null = default;
-        private static readonly Regex Pattern = new Regex("^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$", RegexOptions.Compiled);
 
         private readonly ReadOnlyMemory<char>? value;
 
@@ -217,7 +217,7 @@ namespace Menes
         {
             ValidationResult result = validationResult ?? ValidationResult.ValidResult;
 
-            if ((this.HasJsonElement && (this.JsonElement.ValueKind != JsonValueKind.String || !Pattern.IsMatch(this.JsonElement.GetString()))) || (this.value is ReadOnlyMemory<char> value && !Pattern.IsMatch(value.ToString())))
+            if ((this.HasJsonElement && (this.JsonElement.ValueKind != JsonValueKind.String || !Parse(this.JsonElement.GetString()))) || (this.value is ReadOnlyMemory<char> value && !Parse(value.ToString())))
             {
                 if (level >= ValidationLevel.Basic)
                 {
@@ -266,6 +266,53 @@ namespace Menes
             else
             {
                 writer.WriteNullValue();
+            }
+        }
+
+        private static bool Parse(string? email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper, RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(
+                    email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase,
+                    TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
             }
         }
     }
