7d0e680a463b96eae6dc908f04ea52527fed0146
James Dawson <james.dawson@endjin.com>
2025-06-09 14:38:12 +0100

    Migrate to ZeroFailed

---
+++ .zf/config.ps1
@@ -0,0 +1,68 @@
+<#
+This example demonstrates a software build process using the 'ZeroFailed.Build.DotNet' extension
+to provide the features needed when building a .NET solutions.
+#>
+
+$zerofailedExtensions = @(
+    @{
+        # References the extension from its GitHub repository. If not already installed, use latest version from 'main' will be downloaded.
+        Name = "ZeroFailed.Build.DotNet"
+        GitRepository = "https://github.com/zerofailed/ZeroFailed.Build.DotNet"
+        GitRef = "main"
+    }
+)
+
+# Load the tasks and process
+. ZeroFailed.tasks -ZfPath $here/.zf
+
+
+#
+# Build process control options
+#
+$SkipInit = $false
+$SkipVersion = $false
+$SkipBuild = $false
+$CleanBuild = $Clean
+$SkipTest = $false
+$SkipTestReport = $false
+$SkipAnalysis = $false
+$SkipPackage = $false
+
+#
+# Build process configuration
+#
+$SolutionToBuild = (Resolve-Path (Join-Path $here "Solutions\Menes.sln")).Path
+$ProjectsToPublish = @()
+$NuSpecFilesToPackage = @()
+$NugetPublishSource = property ZF_NUGET_PUBLISH_SOURCE "$here/_local-nuget-feed"
+$IncludeAssembliesInCodeCoverage = "Menes*"
+$ExcludeAssembliesInCodeCoverage = ""
+
+
+# Customise the build process
+
+task . FullBuild
+
+
+#
+# Build Process Extensibility Points - uncomment and implement as required
+#
+
+# task RunFirst {}
+# task PreInit {}
+# task PostInit {}
+# task PreVersion {}
+# task PostVersion {}
+# task PreBuild {}
+# task PostBuild {}
+# task PreTest {}
+# task PostTest {}
+# task PreTestReport {}
+# task PostTestReport {}
+# task PreAnalysis {}
+# task PostAnalysis {}
+# task PrePackage {}
+# task PostPackage {}
+# task PrePublish {}
+# task PostPublish {}
+# task RunLast {}
